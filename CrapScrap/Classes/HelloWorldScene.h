95fef45abb5a105d25a93829b9193a14b664997f
TSITCL\s162129 <taka1122judo@gmail.com>
2017-04-07 12:11:35 +0900

    no message

--- CrapScrap/Classes/HelloWorldScene.h
+++ CrapScrap/Classes/HelloWorldScene.h
@@ -3,15 +3,19 @@
 
 #include "cocos2d.h"
 
+// == 定数の宣言 ======================
+// ウインドウサイズ
+const int WINDOW_H = 640;
+const int WINDOW_W = 960;
+
+// == クラスの宣言 ====================
 class HelloWorld : public cocos2d::Layer
 {
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
 };
